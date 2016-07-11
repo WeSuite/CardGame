130f9e45e01a79f9051d1cbc600d921cf5c6254a
Liezel G <liezelg@Liezels-MacBook-Pro-2.local>
2016-07-11 16:30:08 -0400

    Add WS API Login call

--- src/app/card/card.component.ts
+++ src/app/card/card.component.ts
@@ -15,8 +15,16 @@ import {Card} from "./card";
 export class CardComponent implements OnInit {
 
   cards: Array<Card>;
+  postLoginUser:string;
+
   constructor(cardService: CardsService) {
     cardService.getCards().subscribe(res => this.cards = res);
+    
+    cardService.loginUser().subscribe(//call the post
+                data => this.postLoginUser = JSON.stringify(data), // put the data returned from the server in our variable
+                error => console.log("Error HTTP Post Service"), // in case of failure show this message
+                () => console.log("Login Post !")//run this code in all cases
+            );
   }
   ngOnInit() {
   }
--- src/app/cards.service.ts
+++ src/app/cards.service.ts
@@ -1,17 +1,27 @@
 import { Injectable } from '@angular/core';
 import {Card} from './card/card'
-import {Http} from '@angular/http';
+import {Http, Headers, Response, RequestOptions} from '@angular/http';
 import 'rxjs/Rx';
+import {Observable}     from 'rxjs/Observable';
 
 @Injectable()
 export class CardsService {
 
+  private actionUrl: string;
+  private headers: Headers;
+  private options: RequestOptions;
+
   constructor(public http: Http) {
     console.log('Card Service created.', http);
+
+    this.actionUrl = 'http://wesuite.biz:81/Opportunityapi/json/UserLogin';
+    this.headers = new Headers({ 'Content-Type': 'application/x-www-form-urlencoded' });
+    this.options = new RequestOptions({ headers: this.headers, method: "post" });
   }
 
   getCards()
   {
+    
      var url = "https://api.magicthegathering.io/v1/cards?page=5&pageSize=3";
     // var cards = Array<Card>();
     //  var c = new Array<Card>();
@@ -32,5 +42,21 @@ export class CardsService {
          return result;
        });
   }
+
+  loginUser()
+  {
+    var userData = {username: 'jack', password: 'jack'};
+    console.log("start ws api");
+     let toAdd = JSON.stringify(userData);
+    return this.http.post(this.actionUrl, toAdd, this.options)
+            .map(res => res.json())
+            .catch(this.handleError);
+    //console.log("end ws api");
+  }
+
+  private handleError (error: Response) {
+        console.error(error);
+        return Observable.throw(error.json().error || ' error');
+    }
 
 }
